2669292697035a6ed0621c2934b379cccc026697
周素华 <zhoushzhou@gmail.com>
2022-05-16 16:26:17 +0800

    https://developer.apple.com/tutorials/swiftui/creating-and-combining-views

---
+++ xzl-patient-swift/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  xzl-patient-swift
+//
+//  Created by 周素华 on 2022/5/1620.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
