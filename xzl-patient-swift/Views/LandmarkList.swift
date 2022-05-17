92c9a6419a31dce3ca38fc245bb5baa92048f2ef
周素华 <zhoushzhou@gmail.com>
2022-05-17 18:39:44 +0800

    no message

--- xzl-patient-swift/Views/LandmarkList.swift
+++ xzl-patient-swift/Views/LandmarkList.swift
@@ -8,17 +8,29 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    
+    @State private var showFavoritesOnly = true
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly && landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
         NavigationView {
             
-            List(landmarks) { Landmark in
-                
-                NavigationLink {
-                    LandmarkDetail(landmark: Landmark)
-                } label: {
-                    LandmarkRow(landmark: Landmark)
-                }
-            }
+//            List(landmarks) { landmark in
+//
+//                NavigationLink {
+//                    LandmarkDetail(landmark: landmark)
+//                } label: {
+//                    LandmarkRow(landmark: landmark)
+//                }
+//            }
+            
+            
+            
             .navigationTitle("Landmarks")
         }
     }
@@ -27,7 +39,7 @@ struct LandmarkList: View {
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         
-        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max", "iPhone 12 pro"], id: \.self) { deviceName in
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
                     LandmarkList()
                         .previewDevice(PreviewDevice(rawValue: deviceName))
                         .previewDisplayName(deviceName)
